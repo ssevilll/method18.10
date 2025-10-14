ba7af6d1897b5fae6466adc1a551ce8863a7d72d
ssevilll <sevilsb@code.edu.az>
2025-10-14 18:05:25 +0400

    initial

---
+++ method18.10/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace method18._10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 5 };
+            foreach (int i in SquareOfElements(nums))
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static int[] SquareOfElements(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = arr[i] * arr[i];
+            }
+            return arr;
+        }
+    }
+}
